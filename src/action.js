f7275c252e8a4c51f74aecda01fc79d5b76a4777
Nguyen Duy Cuong <cuongw.me@gmail.com>
2020-02-04 15:13:40 +0700

    Integrate basic redux

---
+++ src/action.js
@@ -0,0 +1,9 @@
+export const saveScreen1Data = data => ({
+  type: '@screen1/save',
+  payload: data,
+});
+
+export const saveScreen2Data = data => ({
+  type: '@screen2/save',
+  payload: data,
+});
